0c6c0e74c115856d6f778bab35bf44d1392865c8
IijimaKaoru <k021g1465@g.neec.ac.jp>
2022-04-26 13:58:33 +0900

    ゴール処理おけ

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,36 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour{
     public float power = 2;
     public Rigidbody rigidbody;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
-
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
+    void Update(){
+        if (Input.GetKey(KeyCode.UpArrow)){
             rigidbody.AddForce(new Vector3(0, 0, 1) * power);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
+        if (Input.GetKey(KeyCode.LeftArrow)){
             rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
+        if (Input.GetKey(KeyCode.RightArrow)){
             rigidbody.AddForce(new Vector3(1, 0, 0) * power);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
+        if (Input.GetKey(KeyCode.DownArrow)){
             rigidbody.AddForce(new Vector3(0, 0, -1) * power);
         }
     }
