39119ed4f7a7498863b614125fb703aea8ee7ec9
IijimaKaoru <k021g1465@g.neec.ac.jp>
2022-05-31 16:05:40 +0900

    korokoro完成

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject clearText;
+    public GameObject nextButton;
+    public AudioSource audioSource;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("ÉSÅ[ÉãÅI");
+
+        clearText.SetActive(true);
+        nextButton.SetActive(true);
+        audioSource.Play();
     }
     // Start is called before the first frame update
     void Start()
