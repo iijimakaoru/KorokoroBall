e3269ab85fde2992e95b5cb4bb91e7a1e7fc7079
IijimaKaoru <k021g1465@g.neec.ac.jp>
2023-01-11 14:42:02 +0900

    hogeですよ～

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
     public float power = 15;
+    private float hoge = 0;
     public Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start(){
